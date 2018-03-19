d48ef11f782b79c7aa2ffe5fc9dab9d2f2c7cf41
heraldohuerta <heraldo.huerta@gmail.com>
2018-03-19 11:58:32 -0300

    OK Ultimo

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
 
-
-  get 'companies/index'
-
-  Rails.application.routes.draw do
-     devise_for :users, controllers: {
-       sessions: 'users/sessions',
-       registrations: 'users/registrations'
-     }
-   end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
 
-  root to: "companies#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'companies/index'
+
+root to: "companies#index"
+
 end
